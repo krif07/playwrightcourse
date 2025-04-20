70c8e0d1a18a10b27adb0a64d6f326e0e90ea766
Cristian Fernando Dávila López <krif07@gmail.com>
2025-04-20 10:48:23 -0500

    Reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,11 @@ export default defineConfig<TestOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['json', {outputFile: 'test-results/jsonReport.js'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
